bfd33ed68db108cda11096d4420adf35ef5d99aa
byanofsky <byanofsky@me.com>
2017-06-19 09:26:07 -0700

    Have config file exist same level as default-config

--- course_catalog/course_catalog/course_catalog.py
+++ course_catalog/course_catalog/course_catalog.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Attempt to use instance/config.py. If does not exist, uses default.config
 app.config.from_pyfile('default_config.py')
-app.config.from_pyfile('./instance/config.py', silent=True)
+app.config.from_pyfile('config.py', silent=True)
 # Create database instance
 db = SQLAlchemy(app)
 
